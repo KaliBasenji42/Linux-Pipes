47b9063caaa4e7dc3f269e1498a8a0fa8e8d9d92
KaliBasenji42 <jacobdelzer369@gmail.com>
2025-03-29 20:50:25 -0600

    Vars and basic setup

---
+++ __main__.py
@@ -0,0 +1,11 @@
+import sys
+
+# Variables
+
+run = False # Run Main Loop
+
+recordPath = 'record.txt' # Path to record file (for High Score and such)
+
+gridSize = (20, 10) # Gam Grid Size
+
+# Pre Loop
